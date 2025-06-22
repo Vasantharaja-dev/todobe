c13c6a06fa306f9f8560ba38eae9089889a45429
Vasantharaja-dev <vasantharajavijayakumar@gmail.com>
2025-06-22 13:46:09 +0530

    add cors

--- api/index.js
+++ api/index.js
@@ -7,7 +7,11 @@ import todoRoutes from './routes/todoRoutes.js'
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type']
+}));
 app.use(express.json());
 
 // Routes
